47834169cfab208062fa9a82ea4591621c3d6891
Vildan Çetin <vildan.cetin@ug.bilkent.edu.tr>
2023-05-09 03:28:32 +0300

    Added some code on the LoginActivity. Please check the TODOs I added and either guide me on how to code it or implement it.

--- app/src/main/java/com/ungratz/okunurmu/MentorSignUpActivity.java
+++ app/src/main/java/com/ungratz/okunurmu/MentorSignUpActivity.java
@@ -1,11 +1,7 @@
 package com.ungratz.okunurmu;
 
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MentorSignUpActivity extends AppCompatActivity {
-    ImageView imageView;
-
 
 }
