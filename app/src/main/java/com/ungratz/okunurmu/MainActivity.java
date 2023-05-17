3f9e3424a26badafc7b289a80c6021a26edec30e
Artun Berke Gül <artunberkegul2003@gmail.com>
2023-05-17 19:13:42 +0300

    commiting with storage purposes

--- app/src/main/java/com/ungratz/okunurmu/MainActivity.java
+++ app/src/main/java/com/ungratz/okunurmu/MainActivity.java
@@ -2,22 +2,29 @@ package com.ungratz.okunurmu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ungratz.okunurmu.databinding.LaunchPageBinding;
+import com.ungratz.okunurmu.databinding.MainActivityBinding;
 
 public class MainActivity extends Activity {
 
     private MainActivity ma = this;
 
-    private LaunchPageBinding binding;
+    private MainActivityBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = LaunchPageBinding.inflate(getLayoutInflater());
+        binding = MainActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.homeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
     }
 
